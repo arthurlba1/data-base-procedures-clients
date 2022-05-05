57a3df09d3223860287cc6d9a7b98173b59c52a4
arthurlba1 <1arthurluiz1@gmail.com>
2022-05-04 21:08:12 -0300

    initialized card title template

--- frontend/src/components/ClientCardComponent.vue
+++ frontend/src/components/ClientCardComponent.vue
@@ -1,7 +1,22 @@
 <template>
-    <div>
-        <h1>Clientes</h1>
-    </div>
+    <v-card
+        width="900"
+        elevation="2"
+    >
+        <v-card-title class="pa-0">
+            <v-toolbar
+            flat
+            color="deep-purple"
+            >
+                <v-toolbar-title>
+                    <v-icon left>
+                        mdi-account-multiple
+                    </v-icon>
+                    Clients
+                </v-toolbar-title>
+            </v-toolbar>
+        </v-card-title>
+    </v-card>
 </template>
 
 <script>
