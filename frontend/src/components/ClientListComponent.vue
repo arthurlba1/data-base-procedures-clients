395ec0ce6ebd7a7ab49efb7297ac9614295a07cb
arthurlba1 <1arthurluiz1@gmail.com>
2022-05-08 19:38:47 -0300

    List of clients component

--- frontend/src/components/ClientListComponent.vue
+++ frontend/src/components/ClientListComponent.vue
@@ -1,9 +1,31 @@
 <template>
-    <h1> Test </h1>
+    <v-row no-gutters>
+        <v-col cols="12">
+            <v-list-item>
+                <v-container class="pa-0">
+                    <client-sub-list-component
+                        v-for="(name, index) in clients"
+                        :key="index"
+                        :clientList="name"
+                    />
+                </v-container>
+            </v-list-item>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
+import ClientSubListComponent from './ClientSubListComponent.vue'
+
 export default {
-    name: 'ClientListComponent'
+
+    name: 'ClientListComponent',
+    components: { ClientSubListComponent },
+    props: {
+        clients: {
+            type: Object,
+            required: true
+        }
+    },
 }
 </script>
