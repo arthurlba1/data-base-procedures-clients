dbb1095ba8e321349914e0e211417e62e10fe065
arthurlba1 <1arthurluiz1@gmail.com>
2022-05-07 22:50:12 -0300

    Creating menu to change view

---
+++ frontend/src/components/ClientMenuComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container fluid>
+        <v-row align="center">
+            <v-col
+                class="d-flex"
+                cols="12"
+                sm="6"
+            >
+                <v-select
+                :items="crud"
+                label="Clients Options"
+                prepend-icon="mdi-account-details"
+                />
+            </v-col>
+        </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+    name: 'ClientMenuComponent',
+    props: {
+        crud: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
