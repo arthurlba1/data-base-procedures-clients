cfd990ec782f48de0e407b18052a9ef3e06109ba
arthurlba1 <1arthurluiz1@gmail.com>
2022-05-08 12:43:15 -0300

    router icon component created to change the router

---
+++ frontend/src/components/RouterIconComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-tooltip
+        bottom
+        opne-delay="300"
+    >
+        <template v-slot:activator="{ on }">
+            <v-btn
+                icon
+                :to="url"
+                v-on="on"
+            >
+                <v-icon>{{ iconName }}</v-icon>
+            </v-btn>
+        </template>
+        <span>{{iconLabel}}</span>
+    </v-tooltip>
+</template>
+
+<script>
+export default {
+    name: 'RouterIconComponent',
+    props: {
+        iconName: {
+            type: String,
+            required: true
+        },
+        iconLabel: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    }    
+}
+</script>
