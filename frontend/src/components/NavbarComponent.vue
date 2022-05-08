fd85042b6d27bf95b0df79804da73b700f1c856d
arthurlba1 <1arthurluiz1@gmail.com>
2022-05-08 18:31:26 -0300

    navbar component finalized

--- frontend/src/components/NavbarComponent.vue
+++ frontend/src/components/NavbarComponent.vue
@@ -1,8 +1,53 @@
 <template>
-
+    <v-app-bar
+        class="deep-purple darken-2 white--text"
+        app
+    >
+        <v-toolbar-title
+            class="mr-3"
+            style="cursor: pointer"
+            @click="goToHomeView"
+        >
+            Future Orders
+        </v-toolbar-title>
+        <v-spacer />
+        <router-icon-component
+            iconName="mdi-account"
+            iconLabel="Clients"
+            url="/clients-list"
+        />
+        <router-icon-component
+            iconName="mdi-account-plus"
+            iconLabel="Create Client"
+            url="/create-list"
+        />
+        <router-icon-component
+            iconName="mdi-account-edit"
+            iconLabel="Update Client"
+            url="/update-client"
+        />
+        <router-icon-component
+            iconName="mdi-account-remove"
+            iconLabel="Delete Client"
+            url="/delete-client"
+        />
+    </v-app-bar>
 </template>
 
 <script>
+import RouterIconComponent from '../components/RouterIconComponent.vue'
+
   export default {
+    name: "NavbarComponent",
+    components: {
+        RouterIconComponent
+    },
+    methods: {
+        goToHomeView: function () {
+            if (this.$router.currentRoute.name !== "/") {
+                this.$router.push({name: "/"})
+            }
+        }
+    }
   }
 </script>
